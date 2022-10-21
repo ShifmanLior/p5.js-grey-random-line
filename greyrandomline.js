f3d1e8a9b01babac32f509fc713eb5928d55ed03
ShifmanLior <38461249+ShifmanLior@users.noreply.github.com>
2022-10-22 00:27:08 +0300

    Create greyrandomline.js

---
+++ greyrandomline.js
@@ -0,0 +1,50 @@
+function setup() {
+  createCanvas(400, 400);
+  frameRate(4);
+
+}
+
+function draw() {
+
+
+  background(255);
+  strokeWeight(0);
+  translate (0 ,0);
+
+  let rectalheight = 1.5
+  let rectalwidth = random(0, 200)
+
+  fill(133,133,133)
+  rect(0, rectalheight, random(0, 400), rectalheight);
+  rect(0, rectalheight*10, random(0, 400), rectalheight);
+  rect(0, rectalheight*20, random(0, 400), rectalheight);
+  rect(0, rectalheight*30, random(0, 400), rectalheight);
+  rect(0, rectalheight*40, random(0, 400), rectalheight);
+  rect(0, rectalheight*50, random(0, 400), rectalheight);
+  rect(0, rectalheight*60, random(0, 400), rectalheight);
+  rect(0, rectalheight*70, random(0, 400), rectalheight);
+  rect(0, rectalheight*80, random(0, 400), rectalheight);
+  rect(0, rectalheight*90, random(0, 400), rectalheight);
+  rect(0, rectalheight*100, random(0, 400), rectalheight);
+  rect(0, rectalheight*110, random(0, 400), rectalheight);
+  rect(0, rectalheight*120, random(0, 400), rectalheight);
+  rect(0, rectalheight*130, random(0, 400), rectalheight);
+  rect(0, rectalheight*140, random(0, 400), rectalheight);
+  rect(0, rectalheight*150, random(0, 400), rectalheight);
+  rect(0, rectalheight*160, random(0, 400), rectalheight);
+  rect(0, rectalheight*170, random(0, 400), rectalheight);
+  rect(0, rectalheight*180, random(0, 400), rectalheight);
+  rect(0, rectalheight*190, random(0, 400), rectalheight);
+  rect(0, rectalheight*200, random(0, 400), rectalheight);
+  rect(0, rectalheight*210, random(0, 400), rectalheight);
+  rect(0, rectalheight*220, random(0, 400), rectalheight);
+  rect(0, rectalheight*230, random(0, 400), rectalheight);
+  rect(0, rectalheight*240, random(0, 400), rectalheight);
+  rect(0, rectalheight*250, random(0, 400), rectalheight);
+  rect(0, rectalheight*260, random(0, 400), rectalheight);
+  rect(0, rectalheight*270, random(0, 400), rectalheight);
+  rect(0, rectalheight*280, random(0, 400), rectalheight);
+  rect(0, rectalheight*290, random(0, 400), rectalheight);
+  rect(0, rectalheight*300, random(0, 400), rectalheight);
+
+}
